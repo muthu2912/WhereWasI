f53c9ac30f97e1beab6bb7cf0788d65cf6e85616
Muthukumaran <mthkumaran08@gmail.com>
2024-03-29 19:45:11 +0530

    competed PlacesVisitedFragment

--- app/src/main/java/view/fragment/PlacesVisitedFragment.kt
+++ app/src/main/java/view/fragment/PlacesVisitedFragment.kt
@@ -1,15 +1,35 @@
 package view.fragment
 
+import adapter.CardLocationInfoAdapter
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.smk.wherewasi.R
 
 class PlacesVisitedFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val locHistoryRecycler =
+            inflater.inflate(R.layout.fragment_places_visited,container,false) as RecyclerView
+        locHistoryRecycler.layoutManager = GridLayoutManager(activity,1)
+        val listener = object  : CardLocationInfoAdapter.Listener{
+            override fun onClick(position: Int) {
+                Toast.makeText(context, "Item clicked at position: $position", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+        }
+        locHistoryRecycler.adapter = CardLocationInfoAdapter(listener)
+        return locHistoryRecycler
     }
 
 }
